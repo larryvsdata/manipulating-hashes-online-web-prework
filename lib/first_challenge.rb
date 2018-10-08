1e975325ec223c0d2f8822168f507e24183859dc
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-08 00:59:05 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,13 @@ def first_challenge
   #your code here
   
   contacts.each do
+    |person, data|
     
+    if person == "Freddy Mercury"
+      data = data.delete_if
+    end 
+    
+  end
 
 
   #remember to return your newly altered contacts hash!
